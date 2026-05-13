a5c3f59e09955eee1e0bf340c1249fda68f3645b
it252070-sk <it252070@trident.ac.jp>
2026-05-13 19:33:11 +0900

    Update DeadLine完成
    敵感知完成

--- Assets/Script/DeadLine.cs
+++ Assets/Script/DeadLine.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class DeadLine : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // インスペクターでGameManagerをドラッグ＆ドロップして紐付ける
+    public GameManager gameManager;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        // 接触した相手が「Enemy」というタグを持っていたら
+        if (collision.CompareTag("Enemy"))
+        {
+            gameManager.EnemyReachedLeft();
+
+            // 到達した敵自身は消しておく
+            Destroy(collision.gameObject);
+        }
     }
 }
