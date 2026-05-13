01318f9f4dbbc89c31234d27f11468e44ae69b27
it252070-sk <it252070@trident.ac.jp>
2026-05-13 18:50:23 +0900

    Update ResultScene
    リザルトシーンのスクリプト完成

--- Assets/Script/Scene/ResultScene.cs
+++ Assets/Script/Scene/ResultScene.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class ResultScene : MonoBehaviour
 {
+    public GameObject resultClearPanel;
+    public GameObject resultGameOverPanel;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        // 両方消す
+        resultClearPanel.SetActive(false);
+        resultGameOverPanel.SetActive(false);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // GameManagerの判定を見て表示を分ける
+        if (GameManager.isClear)
+        {
+            // クリアパネルを表示
+            resultClearPanel.SetActive(true);
+        }
+        else
+        {
+            // ゲームオーバーパネルを表示
+            resultGameOverPanel.SetActive(true);
+        }
     }
 }
