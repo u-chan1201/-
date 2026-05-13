bca4b1c5fc889fb6d91f4055bbbda6cb8d31d225
it252070-sk <it252070@trident.ac.jp>
2026-05-13 23:27:14 +0900

    ResultSceneでスコア反映
    リザルトでスコアを反映させた

--- Assets/Script/Scene/ResultScene.cs
+++ Assets/Script/Scene/ResultScene.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,12 +7,27 @@ public class ResultScene : MonoBehaviour
     public GameObject resultClearPanel;
     public GameObject resultGameOverPanel;
 
+    public TextMeshProUGUI scoreTextClear;
+    public TextMeshProUGUI scoreTextGameOver;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // 両方消す
         resultClearPanel.SetActive(false);
         resultGameOverPanel.SetActive(false);
+
+        // スコアを表示
+        string scoreStr = "Score: " + GameManager.finalScore.ToString();
+
+        if (scoreTextClear != null)
+        {
+            scoreTextClear.text = scoreStr;
+        }
+        if (scoreTextGameOver != null)
+        {
+            scoreTextGameOver.text = scoreStr;
+        }
 
         // GameManagerの判定を見て表示を分ける
         if (GameManager.isClear)
@@ -35,8 +51,9 @@ public class ResultScene : MonoBehaviour
     // リスタート
     public void RestartGame()
     {
-        // 判定用の変数をリセットしてから遷移）
+        // 判定用の変数をリセットしてから遷移
         GameManager.isClear = false;
+        GameManager.finalScore = 0;
         SceneManager.LoadScene("GamePlayScene");
     }
 }
